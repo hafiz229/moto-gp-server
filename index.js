94310dcdb6963fe962002749b7df94a0219f50cf
Hafiz <hasnathafiz229>
2021-11-11 09:18:25 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require("mongodb");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// add connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5rymw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello MotoGP!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
